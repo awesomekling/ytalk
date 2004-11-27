7833b82b6c37fc94520e777844ebc7a863e91d65
Andreas Kling <awesomekling@gmail.com>
2004-11-27 15:28:40 +0000

    Don't do anything if scrollback limit is zero.
    git-svn-id: file:///Users/kling/src/protoman/svn/trunk/ytalk@443 8120b729-f3fe-0310-9465-b9035d29392c

--- src/scroll.c
+++ src/scroll.c
@@ -9,27 +9,31 @@ long int scrollback_lines = 100;
 void
 init_scroll(yuser *user)
 {
-	user->scrollback = (yachar **) get_mem(scrollback_lines * sizeof(yachar *));
-	memset(user->scrollback, 0, scrollback_lines * sizeof(yachar *));
-	user->scrollback[0] = NULL;
-	user->scrollpos = 0;
+	if (scrollback_lines > 0) {
+		user->scrollback = (yachar **) get_mem(scrollback_lines * sizeof(yachar *));
+		memset(user->scrollback, 0, scrollback_lines * sizeof(yachar *));
+		user->scrollback[0] = NULL;
+		user->scrollpos = 0;
+	}
 }
 
 void
 free_scroll(yuser *user)
 {
 	long int i;
-	if (user->scrollback != NULL) {
-		for (i = 0; (i < scrollback_lines) && (user->scrollback[i]); i++)
-			free_mem(user->scrollback[i]);
-		free_mem(user->scrollback);
+	if (scrollback_lines > 0) {
+		if (user->scrollback != NULL) {
+			for (i = 0; (i < scrollback_lines) && (user->scrollback[i]); i++)
+				free_mem(user->scrollback[i]);
+			free_mem(user->scrollback);
+		}
 	}
 }
 
 void
 scroll_up(yuser *user)
 {
-	if (user->scrollback[0] == NULL)
+	if (scrollback_lines == 0 || user->scrollback[0] == NULL)
 		return;
 
 	if (user->scroll == 0)
@@ -49,7 +53,7 @@ scroll_up(yuser *user)
 void
 scroll_down(yuser *user)
 {
-	if (user->scrollback[0] == NULL)
+	if (scrollback_lines == 0 || user->scrollback[0] == NULL)
 		return;
 
 	if ((user->scrollpos > (scrollback_lines - (int) user->rows - 1)) ||
