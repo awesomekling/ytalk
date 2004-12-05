b28757559813a328372c5db5415cbc3a142d23cb
Andreas Kling <awesomekling@gmail.com>
2004-12-05 18:46:36 +0000

    Die unless we have gettext.
    git-svn-id: file:///Users/kling/src/protoman/svn/trunk/ytalk@572 8120b729-f3fe-0310-9465-b9035d29392c

--- autogen.sh
+++ autogen.sh
@@ -11,7 +11,8 @@ echo "Cleaning up..."
 rm -rf aclocal.m4 autom4te.cache configure config.h.in Makefile.in
 
 echo "Setting up gettext..."
-./setup-gettext
+./setup-gettext ||
+	{ echo "FATAL: You need GNU gettext" 2>&1; exit 1; }
 
 echo "Touching \"required\" files..."
 touch ChangeLog
