a1a6bd79439cd07422f2f3786d9ccc9d05483ffb
Andreas Kling <awesomekling@gmail.com>
2004-12-05 15:59:56 +0000

    Added gettext macros depending on ENABLE_NLS.
    git-svn-id: file:///Users/kling/src/protoman/svn/trunk/ytalk@554 8120b729-f3fe-0310-9465-b9035d29392c

--- src/header.h
+++ src/header.h
@@ -16,6 +16,13 @@
 
 #include "config.h"
 
+#ifdef ENABLE_NLS
+#  include <libintl.h>
+#  define _(String) gettext(String)
+#else
+#  define _(String) String
+#endif
+
 #include <sys/types.h>
 #include <sys/param.h>
 
