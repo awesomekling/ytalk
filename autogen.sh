9b4aa08ac8cbd2c31640d8306857c14b5ac89180
Andreas Kling <awesomekling@gmail.com>
2004-12-05 22:23:18 +0000

    Gave the auto-generation script a silly facelift :-)
    git-svn-id: file:///Users/kling/src/protoman/svn/trunk/ytalk@581 8120b729-f3fe-0310-9465-b9035d29392c

--- autogen.sh
+++ autogen.sh
@@ -1,45 +1,69 @@
-#!/bin/sh
+#!/bin/sh -x
 #
 # A compilation of "borrowed" materials.
 
-: ${ACLOCAL=aclocal}
-: ${AUTOMAKE=automake}
-: ${AUTOHEADER=autoheader}
-: ${AUTOCONF=autoconf}
+: ${ACLOCAL="aclocal"}
+: ${AUTOMAKE="automake"}
+: ${AUTOHEADER="autoheader"}
+: ${AUTOCONF="autoconf"}
 
-echo "Cleaning up..."
-rm -rf aclocal.m4 autom4te.cache configure config.h.in Makefile.in
+termcols=`stty -a | sed -n '/columns/s/.*columns \([0-9]*\).*/\1/p'`
+status_offset=`expr $termcols - 5`
+
+function smile {
+	echo -n "[A[$status_offset"
+	echo "G[ [32;01m:)[0m ]"
+}
+
+function frown {
+	echo -n "[A[$status_offset"
+	echo "G[ [31;01m:([0m ]"
+	echo "ERROR: $1"
+	exit 1
+}
+
+echo
+echo "* Autogenerating files for YTalk-4.0.0-cvs *"
+echo
+
+echo "Cleaning up old files"
+rm -rf aclocal.m4 autom4te.cache configure config.h.in Makefile.in \
+	&& smile \
+	|| frown "Couldn't clean up."
 
 echo "Setting up gettext..."
-autopoint -f || \
-	{ echo "FATAL: You need GNU gettext" 2>&1; exit 1; }
+autopoint -f >/dev/null \
+	&& smile \
+	|| frown "You need GNU gettext to compile YTalk."
 
-echo "Touching \"required\" files..."
+echo "Touching ChangeLog..."
 touch ChangeLog
-touch ABOUT-NLS
-test -f ChangeLog || \
-	{ echo "FATAL: Failed to touch ChangeLog" 2>&1; exit 1; }
-test -f ABOUT-NLS || \
-	{ echo "FATAL: Failed to touch ChangeLog" 2>&1; exit 1; }
+test -f "ChangeLog" \
+	&& smile \
+	|| frown "Failed to touch ChangeLog"
 
 echo "Running $ACLOCAL..."
 WANT_ACLOCAL="1.8" $ACLOCAL -I m4 2>/dev/null || exit 1
-test -f aclocal.m4 || \
-	{ echo "FATAL: Failed to generate aclocal.m4" 2>&1; exit 1; }
+test -f "aclocal.m4" \
+	&& smile \
+	|| frown "Failed to generate aclocal.m4"
 
 echo "Running $AUTOHEADER..."
 WANT_AUTOHEADER="2.59" $AUTOHEADER || exit 1
-test -f config.h.in || \
-	{ echo "FATAL: Failed to generate config.h.in" 2>&1; exit 1; }
+test -f "config.h.in" \
+	&& smile \
+	|| frown "Failed to generate config.h.in"
 
 echo "Running $AUTOMAKE..."
 WANT_AUTOMAKE="1.8" $AUTOMAKE --add-missing --copy --ignore-deps || exit 1
-test -f Makefile.in || \
-	{ echo "FATAL: Failed to generate Makefile.in" 2>&1; exit 1; }
+test -f "Makefile.in" \
+	&& smile \
+	|| frown "Failed to generate Makefile.in"
 
 echo "Running $AUTOCONF..."
 WANT_AUTOCONF="2.59" $AUTOCONF || exit 1
-test -f configure || \
-	{ echo "FATAL: Failed to generate configure" 2>&1; exit 1; }
+test -f configure \
+	&& smile \
+	frown "Failed to generate configure"
 
 echo "Your stuff is ready."
