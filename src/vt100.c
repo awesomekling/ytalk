515342cc759f82c023179eb83e7f4bee336e086b
Andreas Kling <awesomekling@gmail.com>
2005-03-05 20:47:14 +0000

    Quit lagging behind -stable and move to GPL!
    git-svn-id: file:///Users/kling/src/protoman/svn/trunk/ytalk@913 8120b729-f3fe-0310-9465-b9035d29392c

--- src/vt100.c
+++ src/vt100.c
@@ -1,4 +1,24 @@
-/* vt100.c - terminal emulation */
+/*
+ * src/vt100.c
+ * VT100 terminal emulation
+ *
+ * YTalk
+ *
+ * Copyright (C) 1990,1992,1993 Britt Yenne
+ * Currently maintained by Andreas Kling
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
 #include "header.h"
 #include "cwin.h"
 
--- src/ymenu.c
+++ src/ymenu.c
@@ -1,6 +1,20 @@
 /*
- * ymenu.c
+ * src/ymenu.c
  *
+ * YTalk
+ *
+ * Copyright (C) 1990,1992,1993 Britt Yenne
+ * Currently maintained by Andreas Kling
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  */
 
