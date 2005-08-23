23079a859f7f58eab3ef2a33efa2d2b492e6a634
Andreas Kling <awesomekling@gmail.com>
2005-08-23 19:55:36 +0000

    Various non-dramatic touchups.
    git-svn-id: file:///Users/kling/src/protoman/svn/trunk/ytalk@986 8120b729-f3fe-0310-9465-b9035d29392c

--- src/mem.h
+++ src/mem.h
@@ -28,23 +28,23 @@ typedef struct __mem_list {
 	struct __mem_list *next;
 } mem_list;
 
-#define get_mem(size) (real_get_mem(size, __LINE__, __FILE__))
+#define get_mem(size) (real_get_mem((size), __LINE__, __FILE__))
 #define realloc_mem(addr, size) (real_realloc_mem((addr), (size), __LINE__, __FILE__))
-#define free_mem(addr) (real_free_mem(addr, __LINE__, __FILE__))
+#define free_mem(addr) (real_free_mem((addr), __LINE__, __FILE__))
 
-yaddr real_get_mem(int, int, char *);
-yaddr real_realloc_mem(yaddr, int, int, char *);
-void real_free_mem(yaddr, int, char *);
-void change_area(mem_list *, yaddr, yaddr, int);
-int get_size(mem_list *, yaddr);
-void clear_all(void);
-mem_list *add_area(mem_list *, yaddr, int, int, char *);
-mem_list *del_area(mem_list *, mem_list *);
+extern yaddr real_get_mem(int, int, char *);
+extern yaddr real_realloc_mem(yaddr, int, int, char *);
+extern void real_free_mem(yaddr, int, char *);
+extern void change_area(mem_list *, yaddr, yaddr, int);
+extern int get_size(mem_list *, yaddr);
+extern void clear_all(void);
+extern mem_list *add_area(mem_list *, yaddr, int, int, char *);
+extern mem_list *del_area(mem_list *, mem_list *);
 #else
-yaddr get_mem(int);
-yaddr realloc_mem(yaddr, int);
 
 #define free_mem(addr) (free(addr))
 
-void free_mem(yaddr);
+extern yaddr get_mem(int);
+extern yaddr realloc_mem(yaddr, int);
+
 #endif /* YTALK_DEBUG */
